eef0617454bb166c71e168d323d46eb4b2047320
skybox <skybox@trioptimum.com>
2018-06-27 05:35:14 -0500

    SecondsArc is running. No text. No animations.

--- app/src/main/java/com/theskyegriffin/prettycoolpolarclock/Arcs/ArcFactory.java
+++ app/src/main/java/com/theskyegriffin/prettycoolpolarclock/Arcs/ArcFactory.java
@@ -1,10 +1,15 @@
 package com.theskyegriffin.prettycoolpolarclock.Arcs;
 
+import android.graphics.Color;
+
 import java.util.ArrayList;
 
 public class ArcFactory implements IArcFactory {
     @Override
     public ArrayList<Arc> buildArcs() {
-        return null;
+        ArrayList<Arc> arcs = new ArrayList<Arc>();
+        arcs.add(new SecondsArc(450, Color.parseColor("#D26767")));
+
+        return arcs;
     }
 }
